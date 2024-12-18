4ac566382945e7d286a6163190cbeba107155c7f
txngUI <tanguy.davidpro85@gmail.com>
2024-12-18 12:03:17 +0100

    style (front-end): update UI of the form add grade

--- apps/web/src/components/forms/add-rate.tsx
+++ apps/web/src/components/forms/add-rate.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Link from "next/link"
+import Link from 'next/link';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -21,12 +21,13 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@web/components/ui/select"
+} from '@web/components/ui/select';
+import { DialogClose } from '@web/components/ui/dialog';
 
 const formSchema = z.object({
   student: z.string(),
   course: z.string(),
-  rate: z.number().int().min(0).max(20),
+  grade: z.number().int().min(0).max(20),
   semester: z.string(),
   academicYear: z.string().regex(/^\d{4}-\d{4}$/),
 });
@@ -38,7 +39,7 @@ export const AddRate = () => {
     defaultValues: {
       student: '',
       course: '',
-      rate: 0,
+      grade: 0,
       semester: '',
       academicYear: '',
     },
@@ -48,39 +49,129 @@ export const AddRate = () => {
     console.log(values);
   }
 
-  // @ts-ignore
   return (
     <div>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="student"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a verified email to display" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="m@example.com">m@example.com</SelectItem>
-                      <SelectItem value="m@google.com">m@google.com</SelectItem>
-                      <SelectItem value="m@support.com">m@support.com</SelectItem>
-                    </SelectContent>
-                  </Select>
-                  <FormDescription>
-                    You can manage email addresses in your{" "}
-                    <Link href="/examples/forms">email settings</Link>.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </form>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 mt-4">
+          <FormField
+            control={form.control}
+            name="student"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Etudiant</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Séléctionner un étudiant" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="course"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Ressource</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Séléctionner une ressource" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="grade"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Note</FormLabel>
+                <Input type="number" placeholder="Note" max={20} min={0} />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="semester"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Semestre</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Séléctionner le semestre" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="course"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Année académique</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Séléctionner l'année académique" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">m@example.com</SelectItem>
+                    <SelectItem value="m@google.com">m@google.com</SelectItem>
+                    <SelectItem value="m@support.com">m@support.com</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-between mx-20">
+            <DialogClose asChild>
+              <Button className="w-32" >Annuler</Button>
+            </DialogClose>
+            <Button className="w-32" type={'submit'}>Ajouter</Button>
+          </div>
+        </form>
       </Form>
     </div>
-);
-}
+  );
+};
