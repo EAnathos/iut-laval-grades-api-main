11b965f2353f419ce91da0be938da912c91eba0d
Antoine JOSSET <a.josset@yahoo.com>
2025-01-08 21:30:07 +0100

    feat: update configuration for Next.js, and enhance Playwright test setup

--- apps/web/src/tests/example.spec.ts
+++ apps/web/src/tests/example.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect, type Page } from '@playwright/test';
 
+test.beforeEach(async ({ page }) => 
+  await page.goto('http://127.0.0.1:3000')
+);
+
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
