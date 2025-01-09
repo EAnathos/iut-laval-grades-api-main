e26c7f8f9f036545205aeaec2e5a72c3a45c69d6
Antoine <113059124+aBgAmeuR@users.noreply.github.com>
2025-01-09 19:33:48 +0100

    Revert "Features/9 ajout de playwright dans le projet"

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,6 +3,8 @@ import { getUser } from "@web/lib/auth";
 
 
 export default async function Home() {
+  const user = await getUser();
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -14,12 +14,12 @@ import { cn } from '@web/lib/utils';
 import { signOutAction } from '@web/actions/auth/auth.actions';
 
 type HeaderProps = {
-  firstName: string;
-  lastName: string;
-  department: string;
+    firstName: string;
+    lastName: string;
+    department: string;
 }
 
-export const Header = ({ firstName, lastName, department }: HeaderProps) => {
+export const Header = ({firstName, lastName, department} : HeaderProps) => {
   const pathname = usePathname();
 
   return (
@@ -79,6 +79,7 @@ export const Header = ({ firstName, lastName, department }: HeaderProps) => {
             'hover:bg-[#A80059] hover:text-muted ',
             pathname === '/students' ? 'bg-[#A80059]' : '',
           )}
+          asChild
           onClick={async () => {
             await signOutAction({
               redirect: true,
@@ -86,8 +87,10 @@ export const Header = ({ firstName, lastName, department }: HeaderProps) => {
             });
           }}
         >
-          <LogOut size={24} className="text-muted" />
-          Déconnexion
+          <Link href="/">
+            <LogOut size={24} className="text-muted" />
+            Déconnexion
+          </Link>
         </Button>
       </div>
     </header>
