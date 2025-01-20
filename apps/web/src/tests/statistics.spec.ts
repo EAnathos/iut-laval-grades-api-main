3d549fccc68a21005fa0245e2257bf188f43d11c
Tanguy D. <tanguy.davidpro85@gmail.com>
2025-01-20 16:10:59 +0100

    final test end to end

--- apps/web/src/tests/statistics.spec.ts
+++ apps/web/src/tests/statistics.spec.ts
@@ -1,33 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => await page.goto('/statistics'));
+test.beforeEach(async ({ page, baseURL }) => {
+  await page.goto('/');
 
-// test('login with valid credentials', async ({ page, baseURL }) => {
-//     await page.fill('input[name="email"]', 'prof@example.com');
-//     await page.fill('input[name="password"]', 'password123');
-  
-//     await page.click('button[type="submit"]');
-  
-//     expect(page.url()).toBe(baseURL + '/app');
-//   });
-  
+  await page.fill('input[name="email"]', 'prof@example.com');
+  await page.fill('input[name="password"]', 'password123');
 
-// test('should display the global statistics', async ({ page }) => {
-//   expect(
-//     await page.isVisible('text="Statistiques Globales"'),
-//   ).toBeTruthy();
-//   expect(
-//     await page.isVisible('text="Moyenne de toutes les notes"'),
-//   ).toBeTruthy();
-//   expect(
-//     await page.isVisible('text="Total des étudiants inscrits"'),
-//   ).toBeTruthy();
-//   expect(
-//     await page.isVisible('text="Total des cours disponibles"'),
-//   ).toBeTruthy();
-//   expect(await page.isVisible('text="Taux de réussite moyen"')).toBeTruthy();
-// });
+  await page.click('button[type="submit"]');
+  await page.waitForSelector('text="Déconnexion"');
 
-// test('should display the courses statistics', async ({ page }) => {
+  await page.goto('/statistics');
+});
 
-// });
+test('should display the global statistics', async ({ page }) => {
+  expect(await page.isVisible('text="Statistiques Globales"')).toBeTruthy();
+  expect(await page.isVisible('text="Moyenne de toutes les notes"')).toBeTruthy();
+  expect(await page.isVisible('text="Total des étudiants inscrits"')).toBeTruthy();
+  expect(await page.isVisible('text="Total des cours disponibles"')).toBeTruthy();
+  expect(await page.isVisible('text="Taux de réussite moyen"')).toBeTruthy();
+});
